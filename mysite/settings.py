fcfa43826741371ba93dab2e2cfc526505dc9a2f
ariel <arieduardo83@gmail.com>
2021-03-10 20:49:49 -0300

    additional files and changes for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,8 +97,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
@@ -133,6 +132,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
