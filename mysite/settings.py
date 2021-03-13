a913009429a9c8a01a9970e33f3a69a18e169de0
ariel <arieduardo83@gmail.com>
2021-03-13 15:59:37 -0300

    listos

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
-        'USER': 'Ariel',
+        'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
