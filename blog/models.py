148ef7aa0793fff4a5bc1e87c4aef274b60c2e8c
ariel <arieduardo83@gmail.com>
2021-03-30 15:50:11 -0300

    img

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,6 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-
-    
-
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
