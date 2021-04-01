a67f850853f63c0f902f8d7f68768e6a0d5c41c7
ariel <arieduardo83@gmail.com>
2021-04-01 15:38:25 -0300

    sub

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title','subtitle' 'text',)
+        fields = ('title','subtitle', 'text',)
 
 class CommentForm(forms.ModelForm):
 
